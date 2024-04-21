7bb144adf9987716755ca0e1efe3bf6ae96601f8
hasanhridoy01 <hasanhridoy1367@gmail.com>
2024-04-21 15:07:08 -0700

    menu section done

--- src/components/AdminDashboard/AdminDashboard.jsx
+++ src/components/AdminDashboard/AdminDashboard.jsx
@@ -18,14 +18,39 @@ import { Avatar, Button, Divider } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import HomeIcon from "@mui/icons-material/Home";
 import Home from "../DashboardComponents/Home/Home";
+import Menu from "@mui/material/Menu";
+import { styled, alpha } from "@mui/material/styles";
+import ArchiveIcon from "@mui/icons-material/Archive";
+import FileCopyIcon from "@mui/icons-material/FileCopy";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import MenuItem from "@mui/material/MenuItem";
+import EditIcon from "@mui/icons-material/Edit";
+import InstallDesktopIcon from '@mui/icons-material/InstallDesktop';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import LockOpenIcon from '@mui/icons-material/LockOpen';
+import PeopleIcon from '@mui/icons-material/People';
+import QuizIcon from '@mui/icons-material/Quiz';
+import VideocamIcon from '@mui/icons-material/Videocam';
 
-const drawerWidth = 240;
+
+const drawerWidth = 260;
 
 function ResponsiveDrawer(props) {
   const { window } = props;
   const [mobileOpen, setMobileOpen] = React.useState(false);
   const [isClosing, setIsClosing] = React.useState(false);
   const [selectedItem, setSelectedItem] = React.useState(null);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   const handleDrawerClose = () => {
     setIsClosing(true);
@@ -41,6 +66,49 @@ function ResponsiveDrawer(props) {
       setMobileOpen(!mobileOpen);
     }
   };
+
+  const StyledMenu = styled((props) => (
+    <Menu
+      elevation={0}
+      anchorOrigin={{
+        vertical: "bottom",
+        horizontal: "right",
+      }}
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      {...props}
+    />
+  ))(({ theme }) => ({
+    "& .MuiPaper-root": {
+      borderRadius: 6,
+      marginTop: theme.spacing(1),
+      minWidth: 180,
+      color:
+        theme.palette.mode === "light"
+          ? "rgb(55, 65, 81)"
+          : theme.palette.grey[300],
+      boxShadow:
+        "rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px",
+      "& .MuiMenu-list": {
+        padding: "4px 0",
+      },
+      "& .MuiMenuItem-root": {
+        "& .MuiSvgIcon-root": {
+          fontSize: 18,
+          color: theme.palette.text.secondary,
+          marginRight: theme.spacing(1.5),
+        },
+        "&:active": {
+          backgroundColor: alpha(
+            theme.palette.primary.main,
+            theme.palette.action.selectedOpacity
+          ),
+        },
+      },
+    },
+  }));
 
   //set listItem content on toolBar....................!
   const handleListItemClick = (index) => {
@@ -105,6 +173,293 @@ function ResponsiveDrawer(props) {
               </ListItemText>
             </ListItemButton>
           </ListItem>
+          {/* set Dropdown */}
+          <ListItem sx={{ marginTop: '0px' }}>
+            <ListItemButton>
+              <div>
+                <Button
+                  id="demo-customized-button"
+                  aria-controls={open ? "demo-customized-menu" : undefined}
+                  aria-haspopup="true"
+                  aria-expanded={open ? "true" : undefined}
+                  variant="outlined"
+                  sx={{
+                    display: "flex", // Ensures the icon and text are aligned properly
+                    alignItems: "center", // Vertically aligns the icon and text
+                    border: "none",
+                    "&:hover": {
+                      border: "none",
+                    },
+                    "&:active": {
+                      border: "none",
+                    },
+                    color: "#000",
+                    padding: "0px",
+                    paddingLeft: "6px",
+                  }}
+                  disableElevation
+                  onClick={handleClick}
+                  startIcon={<InstallDesktopIcon />}
+                  endIcon={<ChevronRightIcon />}
+                >
+                  <ListItemText>
+                    <Typography sx={{ textTransform: "none" }}>Promotions</Typography>
+                  </ListItemText>
+                </Button>
+                <StyledMenu
+                  id="demo-customized-menu"
+                  MenuListProps={{
+                    "aria-labelledby": "demo-customized-button",
+                  }}
+                  anchorEl={anchorEl}
+                  open={open}
+                  onClose={handleClose}
+                >
+                  <MenuItem onClick={handleClose} disableRipple>
+                    <EditIcon />
+                    Edit
+                  </MenuItem>
+                  <MenuItem onClick={handleClose} disableRipple>
+                    <FileCopyIcon />
+                    Duplicate
+                  </MenuItem>
+                  <Divider sx={{ my: 0.5 }} />
+                  <MenuItem onClick={handleClose} disableRipple>
+                    <ArchiveIcon />
+                    Archive
+                  </MenuItem>
+                  <MenuItem onClick={handleClose} disableRipple>
+                    <MoreHorizIcon />
+                    More
+                  </MenuItem>
+                </StyledMenu>
+              </div>
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem sx={{ marginTop: '0px' }}>
+            <ListItemButton>
+              <div>
+                <Button
+                  id="demo-customized-button"
+                  aria-controls={open ? "demo-customized-menu" : undefined}
+                  aria-haspopup="true"
+                  aria-expanded={open ? "true" : undefined}
+                  variant="outlined"
+                  sx={{
+                    display: "flex", // Ensures the icon and text are aligned properly
+                    alignItems: "left", // Vertically aligns the icon and text
+                    border: "none",
+                    "&:hover": {
+                      border: "none",
+                    },
+                    "&:active": {
+                      border: "none",
+                    },
+                    color: "#000",
+                    padding: "0px",
+                    paddingLeft: "6px",
+                  }}
+                  disableElevation
+                  onClick={handleClick}
+                  startIcon={<NotificationsNoneIcon />}
+                  endIcon={<ChevronRightIcon />}
+                >
+                  <ListItemText>
+                    <Typography sx={{ textTransform: "none" }}>PushNotifications</Typography>
+                  </ListItemText>
+                </Button>
+                <StyledMenu
+                  id="demo-customized-menu"
+                  MenuListProps={{
+                    "aria-labelledby": "demo-customized-button",
+                  }}
+                  anchorEl={anchorEl}
+                  open={open}
+                  onClose={handleClose}
+                >
+                  <MenuItem onClick={handleClose} disableRipple>
+                    <EditIcon />
+                    Edit
+                  </MenuItem>
+                  <MenuItem onClick={handleClose} disableRipple>
+                    <FileCopyIcon />
+                    Duplicate
+                  </MenuItem>
+                  <Divider sx={{ my: 0.5 }} />
+                  <MenuItem onClick={handleClose} disableRipple>
+                    <ArchiveIcon />
+                    Archive
+                  </MenuItem>
+                  <MenuItem onClick={handleClose} disableRipple>
+                    <MoreHorizIcon />
+                    More
+                  </MenuItem>
+                </StyledMenu>
+              </div>
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem sx={{ marginTop: '0px' }}>
+            <ListItemButton>
+              <div>
+                <Button
+                  id="demo-customized-button"
+                  aria-controls={open ? "demo-customized-menu" : undefined}
+                  aria-haspopup="true"
+                  aria-expanded={open ? "true" : undefined}
+                  variant="outlined"
+                  sx={{
+                    display: "flex", // Ensures the icon and text are aligned properly
+                    alignItems: "left", // Vertically aligns the icon and text
+                    border: "none",
+                    "&:hover": {
+                      border: "none",
+                    },
+                    "&:active": {
+                      border: "none",
+                    },
+                    color: "#000",
+                    padding: "0px",
+                    paddingLeft: "6px",
+                  }}
+                  disableElevation
+                  onClick={handleClick}
+                  startIcon={<ManageAccountsIcon />}
+                  endIcon={<ChevronRightIcon />}
+                >
+                  <ListItemText>
+                    <Typography sx={{ textTransform: "none" }}>Manage Role</Typography>
+                  </ListItemText>
+                </Button>
+                <StyledMenu
+                  id="demo-customized-menu"
+                  MenuListProps={{
+                    "aria-labelledby": "demo-customized-button",
+                  }}
+                  anchorEl={anchorEl}
+                  open={open}
+                  onClose={handleClose}
+                >
+                  <MenuItem onClick={handleClose} disableRipple>
+                    <EditIcon />
+                    Edit
+                  </MenuItem>
+                  <MenuItem onClick={handleClose} disableRipple>
+                    <FileCopyIcon />
+                    Duplicate
+                  </MenuItem>
+                  <Divider sx={{ my: 0.5 }} />
+                  <MenuItem onClick={handleClose} disableRipple>
+                    <ArchiveIcon />
+                    Archive
+                  </MenuItem>
+                  <MenuItem onClick={handleClose} disableRipple>
+                    <MoreHorizIcon />
+                    More
+                  </MenuItem>
+                </StyledMenu>
+              </div>
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem sx={{ marginTop: '0px' }}>
+            <ListItemButton>
+              <div>
+                <Button
+                  id="demo-customized-button"
+                  aria-controls={open ? "demo-customized-menu" : undefined}
+                  aria-haspopup="true"
+                  aria-expanded={open ? "true" : undefined}
+                  variant="outlined"
+                  sx={{
+                    display: "flex", // Ensures the icon and text are aligned properly
+                    alignItems: "left", // Vertically aligns the icon and text
+                    border: "none",
+                    "&:hover": {
+                      border: "none",
+                    },
+                    "&:active": {
+                      border: "none",
+                    },
+                    color: "#000",
+                    padding: "0px",
+                    paddingLeft: "6px",
+                  }}
+                  disableElevation
+                  onClick={handleClick}
+                  startIcon={<LockOpenIcon />}
+                  endIcon={<ChevronRightIcon />}
+                >
+                  <ListItemText>
+                    <Typography sx={{ textTransform: "none" }}>Manager Access</Typography>
+                  </ListItemText>
+                </Button>
+                <StyledMenu
+                  id="demo-customized-menu"
+                  MenuListProps={{
+                    "aria-labelledby": "demo-customized-button",
+                  }}
+                  anchorEl={anchorEl}
+                  open={open}
+                  onClose={handleClose}
+                >
+                  <MenuItem onClick={handleClose} disableRipple>
+                    <EditIcon />
+                    Edit
+                  </MenuItem>
+                  <MenuItem onClick={handleClose} disableRipple>
+                    <FileCopyIcon />
+                    Duplicate
+                  </MenuItem>
+                  <Divider sx={{ my: 0.5 }} />
+                  <MenuItem onClick={handleClose} disableRipple>
+                    <ArchiveIcon />
+                    Archive
+                  </MenuItem>
+                  <MenuItem onClick={handleClose} disableRipple>
+                    <MoreHorizIcon />
+                    More
+                  </MenuItem>
+                </StyledMenu>
+              </div>
+            </ListItemButton>
+          </ListItem>
+          
+          {/* normal Menu */}
+          <ListItem onClick={() => handleListItemClick(0)}>
+            <ListItemButton>
+              <ListItemIcon sx={{ minWidth: "33px" }}>
+                <PeopleIcon />
+              </ListItemIcon>
+              <ListItemText>
+                <Typography>User Group</Typography>
+              </ListItemText>
+            </ListItemButton>
+          </ListItem>
+          
+          <ListItem onClick={() => handleListItemClick(0)}>
+            <ListItemButton>
+              <ListItemIcon sx={{ minWidth: "33px" }}>
+                <QuizIcon />
+              </ListItemIcon>
+              <ListItemText>
+                <Typography>Manager FQA</Typography>
+              </ListItemText>
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem onClick={() => handleListItemClick(0)}>
+            <ListItemButton>
+              <ListItemIcon sx={{ minWidth: "33px" }}>
+                <VideocamIcon />
+              </ListItemIcon>
+              <ListItemText>
+                <Typography>How to Videos</Typography>
+              </ListItemText>
+            </ListItemButton>
+          </ListItem>
+
         </List>
       </Toolbar>
     </div>
